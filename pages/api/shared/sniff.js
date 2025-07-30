457bf4a5d26c3c31f0a426b3667cb99391c0075b
X-TechPro <xtechpro54@gmail.com>
2025-07-30 14:19:12 +0200

    test

--- pages/api/shared/sniff.js
+++ pages/api/shared/sniff.js
@@ -13,8 +13,8 @@ export function getProviders(type, tmdb_id, season = 1, episode = 1) {
     }
     // Default to movie
     return [
-        { name: 'Vidsrc', url: `https://player.vidsrc.co/embed/movie/${tmdb_id}` },
         { name: 'Vidsrc.vip', url: `https://vidsrc.vip/embed/movie/${tmdb_id}` },
+        { name: 'Vidsrc', url: `https://player.vidsrc.co/embed/movie/${tmdb_id}` },
         { name: 'AutoEmbed', url: `https://player.autoembed.cc/embed/movie/${tmdb_id}` },
         { name: 'UEmbed', url: `https://uembed.site/?id=${tmdb_id}` },
         { name: 'P-Stream', url: `https://iframe.pstream.org/embed/tmdb-movie-${tmdb_id}` },
